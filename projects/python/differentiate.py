b3f54fda4734f5fc2466eb5719b605f06dc8a5f4
Mansur Dudajev <mansor.92@hotmail.com>
2023-09-01 13:39:28 +0200

    finished tasks 1.1 and 1.2

--- projects/python/differentiate.py
+++ projects/python/differentiate.py
@@ -1,11 +1,22 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def differentiate(u, dt):
-    pass
+    du = np.zeros(u.shape)
+    du[0] = (u[1] - u[0])/dt
+    du[-1] = (u[-1] - u[-2])/dt
+    for i in range(1, len(u) - 1):
+        du[i] = (u[i+1] - u[i-1])/2/dt
+    return du
 
 def differentiate_vector(u, dt):
-    pass
+    du = np.zeros(u.shape)
+    du[0] = (u[1] - u[0])/dt
+    du[-1] = (u[-1] - u[-2])/dt
+    du[1:-1] = (u[2:] - u[:-2])/2/dt
+
+    return du
 
 def test_differentiate():
     t = np.linspace(0, 1, 10)
@@ -16,5 +27,18 @@ def test_differentiate():
     assert np.allclose(du1, du2)
 
 if __name__ == '__main__':
+    t = np.linspace(0, 4*np.pi, 1001)
+    dt = t[1] - t[0]
+    y = np.sin(t)
+    dy_ex = np.cos(t)
+    dy = differentiate(y, dt)
+    dy_vec = differentiate_vector(y, dt)
+    plt.plot(t, dy_ex)
+    plt.plot(t, dy, '--')
+    plt.plot(t, dy_vec, ':')
+    plt.legend(["dy exact", "dy using loops", "dy vectorized"])
+    plt.show()
+
+
     test_differentiate()
     
--- projects/python/mesh_function.py
+++ projects/python/mesh_function.py
@@ -1,14 +1,32 @@
 import numpy as np
-
+import matplotlib.pyplot as plt
 
 def mesh_function(f, t):
-    pass
+    f_mesh = np.zeros(t.shape)
+    for i in range(len(t)):
+        f_mesh[i] = f(t[i])
+    return f_mesh
 
 def func(t):
-    pass
+    if t >= 0 and t <= 3:
+        return np.exp(-t)
+    if t > 3 and t <= 4:
+        return np.exp(-3*t)
 
 def test_mesh_function():
     t = np.array([1, 2, 3, 4])
     f = np.array([np.exp(-1), np.exp(-2), np.exp(-3), np.exp(-12)])
     fun = mesh_function(func, t)
     assert np.allclose(fun, f)
+
+
+if __name__ == '__main__':
+    dt = 0.1
+    N = int(4/dt)
+    t = np.linspace(0, 4, N+1)
+    f_mesh = mesh_function(func, t)
+
+    plt.plot(t, f_mesh)
+    plt.xlabel('$x$')
+    plt.ylabel('$f(x)$')
+    plt.show()
